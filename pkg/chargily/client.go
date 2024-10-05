390ad59bf5d5a419874c902a36fb6f1e1a10c277
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-10-05 18:23:12 +0200

    update: using the new request sender created to send the request and minimize the code

--- pkg/chargily/client.go
+++ pkg/chargily/client.go
@@ -1,18 +1,14 @@
 package chargily
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
+	"strings"
 )
 
 // Client is the structure that holds the API key , the endpoint for the Chargily API and the development mode.
 type Client struct {
     apiKey   		string
     endpoint 		string
-    client   		*http.Client // Allows for custom HTTP client configurations if needed
+    rs              RequestSenderI // rs: stands for RequestSender and used to send custom http requests
 	mode            Mode
 }
 
@@ -34,13 +30,15 @@ func NewClient(apiKey , mode string) (*Client, error) {
         return nil, ErrInvalidMode
     }
 
-    fmt.Println(apiKey,"\n", api_baseUrl,"\n", mode)
+
+    //new request sender 
+    requestSender := NewRequestSender(apiKey)
     
     //return the client with it's configurations
     return &Client{
         apiKey:   	apiKey,
         endpoint: 	api_baseUrl,
-        client:   	&http.Client{},
+        rs:         requestSender,
 		mode:       Mode(mode), //test: for testing/development stage , prod: for production applications
     }, nil 
 }
@@ -49,53 +47,14 @@ func NewClient(apiKey , mode string) (*Client, error) {
 
 //retrieve the balance example 
 func (c *Client) GetBalance() (*Balance, error) {
-	// Create context with timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
 
-	// Build the request
-	url := c.endpoint + "balance"
-    fmt.Println(url)
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, err
-	}
+    var balance Balance
+    //send the request 
+    err := c.rs.SendRequest("GET",  strings.Join([]string{c.endpoint, "balance"}, ""), nil, &balance)
 
-	// Add the authorization header
-	req.Header.Set("Authorization", "Bearer "+c.apiKey)
-
-	// Send the request
-	res, err := c.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-
-    
-    // Check if status code is not 200
-    if res.StatusCode != http.StatusOK {
-        // Try to decode the error message from the response body
-        var apiError struct {
-            Message string `json:"message"` 
-        }
-
-        if err := json.NewDecoder(res.Body).Decode(&apiError); err != nil {
-            return nil, fmt.Errorf("failed to fetch balance with status: %s and could not decode error message: %v", res.Status, err)
-        }
-
-        // Return the error message from the response
-        return nil, fmt.Errorf("failed to fetch balance with status: %s, error: %s", res.Status, apiError.Message)
+    if err != nil {
+        return nil, err
     }
-
-
-
-	// Parse the response body
-	var balance Balance
-	if err := json.NewDecoder(res.Body).Decode(&balance); err != nil {
-		return nil, err
-	}
-
 	// Return the parsed balance object
 	return &balance, nil
 }
