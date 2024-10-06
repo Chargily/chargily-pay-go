a81b3179ee3e5ed90c85c4756534845070c03e60
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-10-06 17:11:04 +0200

    feat: retrieve all prices available, GET request , returns some static properties + data array of all prices

--- pkg/chargily/chargily.go
+++ pkg/chargily/chargily.go
@@ -125,3 +125,19 @@ type ProductPrice struct {
 	CreatedAt               int64                        `json:"created_at"`       // The timestamp of when the price was created.
 	UpdatedAt               int64                        `json:"updated_at"`       // The timestamp of when the price was updated.
 }
+
+
+
+type AllPricesResponse struct {
+	Livemode                bool                         `json:"livemode"`	      
+	CurrentPage             int                          `json:"current_page"`
+	Data                    []ProductPrice               `json:"data"` 
+	FirstPageURL            string                       `json:"first_page_url"`
+	LastPage                int                          `json:"last_page"`
+	LastPageURL             string                       `json:"last_page_url"`
+	NextPageURL             *string                      `json:"next_page_url"` 
+	Path                    string                       `json:"path"`
+	PerPage                 int                          `json:"per_page"`
+	PrevPageURL             *string                      `json:"prev_page_url"` 
+	Total                   int                          `json:"total"`
+}
