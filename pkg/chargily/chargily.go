ed7bc49ae1870e9959208e2e08690f60c8bd9dd1
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-10-07 14:14:52 +0200

    adding a struct for updating metadata of the price

--- pkg/chargily/chargily.go
+++ pkg/chargily/chargily.go
@@ -114,6 +114,15 @@ type ProductPriceParams struct {
 }
 
 
+//update PriceMetaData 
+type UpdatePriceMetaDataParams struct {
+    Metadata                 map[string]any              `json:"metadata"`  // Additional information about the price.
+}
+
+
+// ProductPrice represents a price entity.
+
+
 type ProductPrice struct {
 	ID                      string                       `json:"id"`            // The unique identifier of the price.
 	Entity                  string                       `json:"entity"`          // The entity type (e.g., "price").
@@ -121,7 +130,7 @@ type ProductPrice struct {
 	Amount                  int64                        `json:"amount"`          // The price amount in cents.
 	Currency                string                       `json:"currency"`         // The currency code (e.g., "dzd", "usd", "eur").
 	ProductID               string                       `json:"product_id"`      // The ID of the product to which the price applies.
-	Metadata                map[string]any               `json:"metadata"`         // Additional information about the price.
+	Metadata                map[string]any               `json:"metadata,omitempty"`         // Additional information about the price.
 	CreatedAt               int64                        `json:"created_at"`       // The timestamp of when the price was created.
 	UpdatedAt               int64                        `json:"updated_at"`       // The timestamp of when the price was updated.
 }
